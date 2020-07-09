bd17a30b4eda99147c5c0a2c55a5a0a7bb809632
alexrobaina <alexrobainaph@gmail.com>
2020-07-09 11:32:05 -0300

    add try catch on create user and add config database

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,22 +13,27 @@ function createToken(user: IUser) {
 export const signUp = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({
-      message: 'Please, Send your email and password',
-    });
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        message: 'Please, Send your email and password',
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: 'The user already exist' });
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    return res.status(201).json(newUser);
+  } catch (e) {
+    console.error(e);
+    return res.status(500);
   }
-
-  const user = await User.findOne({ email });
-
-  if (user) {
-    return res.status(400).json({ message: 'The user already exist' });
-  }
-  console.log(req.body)
-  const newUser = new User(req.body);
-  await newUser.save();
-
-  return res.status(201).json(newUser);
 };
 
 export const signIn = async (req: Request, res: Response) => {
