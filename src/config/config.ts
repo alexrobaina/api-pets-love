72a394c19303fbb90e218ed69ba4a1bb3363ba4a
alexrobaina <alexrobainaph@gmail.com>
2020-07-09 01:59:37 -0300

    hot fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export default {
   jwrSecret: process.env.JWT_SECRET || 'seedSecreteToken',
   DB: {
-    URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/petsLove-typescript',
+    URI: process.env.DB_MONGO_DB_URL || 'mongodb://localhost:27017/petsLove-typescript',
     USER: '',
     PASSWORD: '',
   },
