5c1464422b82cb02d084ffe31b17ff42eec52530
alexrobaina <alexrobainaph@gmail.com>
2020-09-11 17:32:50 -0300

    add change nodemailer

--- src/controllers/forgotPassword.controler.ts
+++ src/controllers/forgotPassword.controler.ts
@@ -1,4 +1,5 @@
 import * as nodemailer from 'nodemailer';
+import { google } from 'googleapis';
 import jwt from 'jsonwebtoken';
 import config from '../config';
 import Hogan from 'hogan.js';
@@ -40,21 +41,13 @@ export const forgotPassword = async (req: any, res: any, next: any) => {
     secure: true,
     auth: {
       type: 'OAuth2',
-      user: 'app.petslove@gmail.com',
-      clientId: '495233584114-1sjl5g1aubu98oqpg3v4e2qum7kp1gbr.apps.googleusercontent.com',
-      clientSecret: 'pOQswL5We8dRAv8JpS4Ltbqa',
-      refreshToken:
-        '1//04CeXg7MwdEJ0CgYIARAAGAQSNwF-L9IrmqAbaH83G4AX1nOiY_7pGg6EXSCzVtzaZnd08Wqo_6GB_gleCcFl5MWR6FVJudqgngI',
-      accessToken:
-        'ya29.a0AfH6SMBB6i0dKNbWnELGIryKGvQk3qFVOBIEQk1GlePI5_ANSO5RV8pTeaVitSGvQVLmNp4oVybCdksnzzn0mlhD6eFGq4q-0iXbaaHq9w9l3ijpLZftVBPphJeK3ChtLDpVumsrN0pNM65LnMHWwjnP8DvShBYSv-QQ',
+      user: process.env.USER,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN,
     },
   });
-
-  //   USER=app.petslove@gmail.com
-  // CLIENT_ID=495233584114-1sjl5g1aubu98oqpg3v4e2qum7kp1gbr.apps.googleusercontent.com
-  // CLIENT_SECRET=pOQswL5We8dRAv8JpS4Ltbqa
-  // REFRESH_TOKEN=1//04CeXg7MwdEJ0CgYIARAAGAQSNwF-L9IrmqAbaH83G4AX1nOiY_7pGg6EXSCzVtzaZnd08Wqo_6GB_gleCcFl5MWR6FVJudqgngI
-  // ACCESS_TOKEN=ya29.a0AfH6SMBB6i0dKNbWnELGIryKGvQk3qFVOBIEQk1GlePI5_ANSO5RV8pTeaVitSGvQVLmNp4oVybCdksnzzn0mlhD6eFGq4q-0iXbaaHq9w9l3ijpLZftVBPphJeK3ChtLDpVumsrN0pNM65LnMHWwjnP8DvShBYSv-QQ
 
   let mailOptions = {
     from: process.env.USER,
