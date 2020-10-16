110e3fcb74107252ef2679aa125c0e83054f3280
alexrobaina <alexrobainaph@gmail.com>
2020-10-16 14:20:26 -0300

    add DEV variable env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 const result = dotenv.config();
 
-if (result.error) {
+if (result.error && process.env.DEV) {
   console.log('Please set up a .env file with values. See README.md for more info.');
   process.exit(0);
 }
