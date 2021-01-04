e61c7a5c8e45181f3d57798c3a354bdcabbae310
alexrobaina <alexrobainaph@gmail.com>
2021-01-04 20:07:13 -0300

    change config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,20 +1,19 @@
 import * as dotenv from 'dotenv';
 const result = dotenv.config();
 
-// const IS_DEV = process.env.DEV ? true : false;
+const IS_DEV = process.env.DEV ? true : false;
 
-// if (result.error && IS_DEV) {
-//   console.log('Please set up a .env file with values. See README.md for more info.');
-//   process.exit(0);
-// }
+if (result.error && IS_DEV) {
+  console.log('Please set up a .env file with values. See README.md for more info.');
+  process.exit(0);
+}
 
 export default {
   jwrSecret: process.env.JWT_SECRET || ' ',
   DB: {
-    URI:
-      // ? `${process.env.MONGO_DB_PREFIX}${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_NAME}${process.env.MONGO_DB_OPTIONS}`
-      // : `${process.env.MONGO_DB_PREFIX}${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_HOST_PORT}/${process.env.MONGO_DB_NAME}${process.env.MONGO_DB_OPTIONS}`,
-      `${process.env.MONGO_DB_PREFIX}${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_NAME}${process.env.MONGO_DB_OPTIONS}`,
+    URI: !IS_DEV
+      ? `${process.env.MONGO_DB_PREFIX}${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_NAME}${process.env.MONGO_DB_OPTIONS}`
+      : `${process.env.MONGO_DB_PREFIX}${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_HOST_PORT}/${process.env.MONGO_DB_NAME}${process.env.MONGO_DB_OPTIONS}`,
   },
   awsConfig: {
     SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || '',
