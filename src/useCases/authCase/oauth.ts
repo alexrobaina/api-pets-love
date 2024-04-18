b23ac074c6e81d0d455bb61fd6661814fb119579
alexrobaina <alexrobainaph@gmail.com>
2024-04-18 13:25:09 -0300

    fix error endpoint user public

--- src/useCases/authCase/oauth.ts
+++ src/useCases/authCase/oauth.ts
@@ -13,20 +13,22 @@ export const oauth = async (_req: Request, res: Response) => {
       config.google.GOOGLE_CLIENT_ID,
       config.google.GOOGLE_CLIENT_SECRET,
       config.HOST + '/api/auth/callback/google'
-    );
-
-    const scopes = [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/userinfo.profile',
-    ];
-
-    const url = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: scopes,
-    });
-
+      );
+      
+      const scopes = [
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+      ];
+      
+      const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes,
+      });
+      
     return res.status(200).json({
+      ok: true,
       status: 200,
+      error: null,
       location: url,
       message: SUCCESS_RESPONSE,
     });
